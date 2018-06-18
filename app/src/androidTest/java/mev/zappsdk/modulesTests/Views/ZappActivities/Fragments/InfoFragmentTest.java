880739fa78d456cac8eea28d32bb7cf69ad0c37e
ptyagi <ptyagicodecamp@gmail.com>
2018-06-18 14:13:31 -0700

    added sample app

--- app/src/androidTest/java/mev/zappsdk/modulesTests/Views/ZappActivities/Fragments/InfoFragmentTest.java
+++
@@ -1,7 +0,0 @@
-package mev.zappsdk.modulesTests.Views.ZappActivities.Fragments;
-
-/**
- * Created by andrew on 09.04.16.
- */
-public class InfoFragmentTest {
-}
--- app/src/androidTest/java/mev/zappsdk/modulesTests/Views/ZappActivities/ZappIdActivityTest.java
+++
@@ -1,9 +0,0 @@
-package mev.zappsdk.modulesTests.Views.ZappActivities;
-
-/**
- * Created by andrew on 09.04.16.
- */
-
-public class ZappIdActivityTest {
-
-}
